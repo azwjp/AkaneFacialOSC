cce6e506ca80863d663604d389924e2fc137b3d4
Akane Sayama <a@azw.jp>
2022-02-18 00:10:00 +0900

    Sending a test signal

---
+++ Assets/Scripts/FacialCapture.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Lip;
+using System.Linq;
+using System;
+
+public class FacialCapture : MonoBehaviour
+{
+    public OSC osc;
+    private Dictionary<LipShape_v2, float> lipWeight = new Dictionary<LipShape_v2, float>();
+    [SerializeField]
+    private string addressRoot = "/avatar/parameters/";
+    [SerializeField]
+    private string prefix = "facial_";
+    [SerializeField] string Jaw_Right = "Jaw_Right";
+    [SerializeField] string Jaw_Left = "Jaw_Left";
+    [SerializeField] string Jaw_Forward = "Jaw_Forward";
+    [SerializeField] string Jaw_Open = "Jaw_Open";
+    [SerializeField] string Mouth_Ape_Shape = "Mouth_Ape_Shape";
+    [SerializeField] string Mouth_Upper_Right = "Mouth_Upper_Right";
+    [SerializeField] string Mouth_Upper_Left = "Mouth_Upper_Left";
+    [SerializeField] string Mouth_Lower_Right = "Mouth_Lower_Right";
+    [SerializeField] string Mouth_Lower_Left = "Mouth_Lower_Left";
+    [SerializeField] string Mouth_Upper_Overturn = "Mouth_Upper_Overturn";
+    [SerializeField] string Mouth_Lower_Overturn = "Mouth_Lower_Overturn";
+    [SerializeField] string Mouth_Pout = "Mouth_Pout";
+    [SerializeField] string Mouth_Smile_Right = "Mouth_Smile_Right";
+    [SerializeField] string Mouth_Smile_Left = "Mouth_Smile_Left";
+    [SerializeField] string Mouth_Sad_Right = "Mouth_Sad_Right";
+    [SerializeField] string Mouth_Sad_Left = "Mouth_Sad_Left";
+    [SerializeField] string Cheek_Puff_Right = "Cheek_Puff_Right";
+    [SerializeField] string Cheek_Puff_Left = "Cheek_Puff_Left";
+    [SerializeField] string Cheek_Suck = "Cheek_Suck";
+    [SerializeField] string Mouth_Upper_UpRight = "Mouth_Upper_UpRight";
+    [SerializeField] string Mouth_Upper_UpLeft = "Mouth_Upper_UpLeft";
+    [SerializeField] string Mouth_Lower_DownRight = "Mouth_Lower_DownRight";
+    [SerializeField] string Mouth_Lower_DownLeft = "Mouth_Lower_DownLeft";
+    [SerializeField] string Mouth_Upper_Inside = "Mouth_Upper_Inside";
+    [SerializeField] string Mouth_Lower_Inside = "Mouth_Lower_Inside";
+    [SerializeField] string Mouth_Lower_Overlay = "Mouth_Lower_Overlay";
+    [SerializeField] string Tongue_LongStep1 = "Tongue_LongStep1";
+    [SerializeField] string Tongue_LongStep2 = "Tongue_LongStep2";
+    [SerializeField] string Tongue_Down = "Tongue_Down";
+    [SerializeField] string Tongue_Up = "Tongue_Up";
+    [SerializeField] string Tongue_Right = "Tongue_Right";
+    [SerializeField] string Tongue_Left = "Tongue_Left";
+    [SerializeField] string Tongue_Roll = "Tongue_Roll";
+    [SerializeField] string Tongue_UpLeft_Morph = "Tongue_UpLeft_Morph";
+    [SerializeField] string Tongue_UpRight_Morph = "Tongue_UpRight_Morph";
+    [SerializeField] string Tongue_DownLeft_Morph = "Tongue_DownLeft_Morph";
+    [SerializeField] string Tongue_DownRight_Morph = "Tongue_DownRight_Morph";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    SRanipal_Lip_v2.GetLipWeightings(out lipWeight);
+
+        foreach(var param in lipWeight)
+        {
+            var key = GetMappedKey(param.Key);
+            
+            if (string.IsNullOrWhiteSpace(key)) continue;
+
+            var message = new OscMessage();
+            message.address = addressRoot + prefix + param.Key;
+            message.values.Add(param.Value);
+            //osc.Send(message);
+        }
+        var m = new OscMessage();
+        m.address = "/avatar/parameters/facial_JawOpen";
+        m.values.Add(Mathf.Sin(Time.time));
+        osc.Send(m);
+    }
+    string GetMappedKey(LipShape_v2 key)
+    {
+        switch(key)
+        {
+            case LipShape_v2.Jaw_Right:
+                return Jaw_Right;
+            case LipShape_v2.Jaw_Left:
+                return Jaw_Left;
+            case LipShape_v2.Jaw_Forward:
+                return Jaw_Forward;
+            case LipShape_v2.Jaw_Open:
+                return Jaw_Open;
+            case LipShape_v2.Mouth_Ape_Shape:
+                return Mouth_Ape_Shape;
+            case LipShape_v2.Mouth_Upper_Right:
+                return Mouth_Upper_Right;
+            case LipShape_v2.Mouth_Upper_Left:
+                return Mouth_Upper_Left;
+            case LipShape_v2.Mouth_Lower_Right:
+                return Mouth_Lower_Right;
+            case LipShape_v2.Mouth_Lower_Left:
+                return Mouth_Lower_Left;
+            case LipShape_v2.Mouth_Upper_Overturn:
+                return Mouth_Upper_Overturn;
+            case LipShape_v2.Mouth_Lower_Overturn:
+                return Mouth_Lower_Overturn;
+            case LipShape_v2.Mouth_Pout:
+                return Mouth_Pout;
+            case LipShape_v2.Mouth_Smile_Right:
+                return Mouth_Smile_Right;
+            case LipShape_v2.Mouth_Smile_Left:
+                return Mouth_Smile_Left;
+            case LipShape_v2.Mouth_Sad_Right:
+                return Mouth_Sad_Right;
+            case LipShape_v2.Mouth_Sad_Left:
+                return Mouth_Sad_Left;
+            case LipShape_v2.Cheek_Puff_Right:
+                return Cheek_Puff_Right;
+            case LipShape_v2.Cheek_Puff_Left:
+                return Cheek_Puff_Left;
+            case LipShape_v2.Cheek_Suck:
+                return Cheek_Suck;
+            case LipShape_v2.Mouth_Upper_UpRight:
+                return Mouth_Upper_UpRight;
+            case LipShape_v2.Mouth_Upper_UpLeft:
+                return Mouth_Upper_UpLeft;
+            case LipShape_v2.Mouth_Lower_DownRight:
+                return Mouth_Lower_DownRight;
+            case LipShape_v2.Mouth_Lower_DownLeft:
+                return Mouth_Lower_DownLeft;
+            case LipShape_v2.Mouth_Upper_Inside:
+                return Mouth_Upper_Inside;
+            case LipShape_v2.Mouth_Lower_Inside:
+                return Mouth_Lower_Inside;
+            case LipShape_v2.Mouth_Lower_Overlay:
+                return Mouth_Lower_Overlay;
+            case LipShape_v2.Tongue_LongStep1:
+                return Tongue_LongStep1;
+            case LipShape_v2.Tongue_LongStep2:
+                return Tongue_LongStep2;
+            case LipShape_v2.Tongue_Down:
+                return Tongue_Down;
+            case LipShape_v2.Tongue_Up:
+                return Tongue_Up;
+            case LipShape_v2.Tongue_Right:
+                return Tongue_Right;
+            case LipShape_v2.Tongue_Left:
+                return Tongue_Left;
+            case LipShape_v2.Tongue_Roll:
+                return Tongue_Roll;
+            case LipShape_v2.Tongue_UpLeft_Morph:
+                return Tongue_UpLeft_Morph;
+            case LipShape_v2.Tongue_UpRight_Morph:
+                return Tongue_UpRight_Morph;
+            case LipShape_v2.Tongue_DownLeft_Morph:
+                return Tongue_DownLeft_Morph;
+            case LipShape_v2.Tongue_DownRight_Morph:
+                return Tongue_DownRight_Morph;
+            default:
+                return key.ToString();
+        }
+    }
+
+    [Serializable]
+    class VSRKeyMapper
+    {
+        public LipShape_v2 lipShape;
+        public string target;
+    }
+}
