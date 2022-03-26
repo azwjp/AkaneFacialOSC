f9671aa791a0a19479b5be5c978e996c9bd07a22
Akane Sayama <a@azw.jp>
2022-03-26 18:51:37 +0900

    Canceled to use WinUI3, changing to WPF

--- AkaneFacialOSC/MainWindow.xaml.cs
+++ AkaneFacialOSC/MainWindow.xaml.cs
@@ -7,8 +7,11 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using ViveSR.anipal.Eye;
 using Windows.Foundation;
@@ -17,22 +20,97 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace AkaneFacialOSC
+namespace AZW.FacialOSC
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        bool isDirty = false;
+        Rows rows = new Rows();
+
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            var a =Preferences.Load();
+
+            SignalList.DataContext = new ObservableCollection<SignalRow>(rows.originalList);
+
         }
 
+        void GainValue_Inputted(object sender, RoutedEventArgs e)
+        {
+            var ui = sender as TextBox;
+            var row = ui.DataContext as SignalRow;
+        }
+
+        private void Slider_Gain_ValueChanged(object sender, RoutedEventArgs e)
+        {
+            var ui = sender as Slider;
+            var row = ui.DataContext as SignalRow;
+        }
+        public ObservableCollection<SignalRow> Items { get; private set; }
+            = new ObservableCollection<SignalRow>();
+
+        private void Button_ContextCanceled(UIElement sender, RoutedEventArgs args)
+        {
+
+        }
+        private void centerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ui = sender as Button;
+            var row = ui.DataContext as SignalRow;
+
+            switch (row.Center)
+            {
+                case Center.Fixed:
+                    return;
+                case Center.Half:
+                    row.Center = Center.Zero;
+                    break;
+                case Center.Zero:
+                    row.Center = Center.Half;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            //ui.Content = row.Center;
+        }
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             ViveSR.anipal.SRanipal_API.Initial(SRanipal_Eye.ANIPAL_TYPE_EYE_V2, IntPtr.Zero);
             myButton.Content = "Clicked";
         }
+
+        private void myButton_Click3(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public sealed class AccessibleButton : Control
+    {
+        public AccessibleButton()
+        {
+            DefaultStyleKey = typeof(AccessibleButton);
+        }
+
+        public static DependencyProperty LabelProperty = DependencyProperty.Register(
+            "Label", typeof(string), typeof(AccessibleButton),
+            PropertyMetadata.Create(string.Empty));
+
+        public string Label
+        {
+            set { SetValue(LabelProperty, value); }
+            get { return (string)GetValue(LabelProperty); }
+        }
+
+        protected override void OnPointerPressed(PointerRoutedEventArgs e)
+        {
+            Click?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler Click;
     }
 }
---
+++ AkaneFacialOSC/SignalRow.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZW.FacialOSC
+{
+    public class Rows
+    {
+        public ICollection<SignalRow> originalList = new List<SignalRow>();
+
+        public Rows()
+        {
+            foreach (FaceKey key in Enum.GetValues(typeof(FaceKey)))
+            {
+                var faceVal = new SignalRow();
+                faceVal.Key = key;
+                if (FaceKeyUtils.GetDataType(key) == DataType.Gaze) faceVal.Center = Center.Zero;
+
+                originalList.Add(faceVal);
+            }
+        }
+    }
+
+    public class SignalRow : ModelBase
+    {
+        public void InitRow(FaceKey key, bool isOn, float gain, bool isClipping, Center center)
+        {
+            Key = key;
+            IsSending = isOn;
+            Gain = gain;
+            IsClipping = isClipping;
+            Center = center;
+        }
+
+        public FaceKey Key { get; set; }
+
+        private double gain = 1;
+
+        public bool IsSending { get; set; } = true;
+        public bool IsClipping = true;
+
+        public double Gain
+        {
+            get { return gain; }
+            set { 
+                gain = value;
+                OnPropertyChanged(nameof(Gain));
+            }
+        }
+
+        Center center = Center.Fixed;
+        public Center Center
+        {
+            get { return center; }
+            set {
+                center = value;
+                OnPropertyChanged(nameof(Center));
+                switch (value){
+                    case Center.Fixed:
+                        CenterValue = 0.5;
+                        return;
+                    case Center.Zero:
+                        CenterValue = 0;
+                        return;
+                    case Center.Half:
+                        CenterValue = 0.5;
+                        return;
+                    default:
+                        throw new NotImplementedException(nameof(value));
+                }
+            }
+        }
+        public double CenterValue = 0.5;
+    }
+    public class ModelBase : System.ComponentModel.INotifyPropertyChanged
+    {
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(name));
+        }
+    }
+    public enum Center
+    {
+        Fixed, Zero, Half,
+    }
+
+    public class CenterToLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not Center center) return DependencyProperty.UnsetValue;
+
+            switch (center)
+            {
+                case Center.Fixed:
+                case Center.Half:
+                    return "0..1";
+                case Center.Zero:
+                    return "-1..1";
+                default:
+                    throw new NotImplementedException(nameof(value));
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not string str) return DependencyProperty.UnsetValue;
+
+            return Enum.Parse<Center>(str);
+        }
+    }
+    public class CenterToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not Center center) return DependencyProperty.UnsetValue;
+
+            switch (center)
+            {
+                case Center.Fixed:
+                    return Visibility.Collapsed;
+                case Center.Half:
+                case Center.Zero:
+                    return Visibility.Visible;
+                default:
+                    throw new NotImplementedException(nameof(value));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not Visibility v) return DependencyProperty.UnsetValue;
+
+            return v == Visibility.Collapsed ? Center.Fixed : Center.Half;
+        }
+    }
+}
