7788503caa4dde4420013ab16568162f268e1a08
Akane Sayama <a@azw.jp>
2022-04-12 00:54:53 +0900

    Fix: Some signals from the trackers for debug were not useful

--- AkaneFacialOSC/Tracking/MockLipTracker.cs
+++ AkaneFacialOSC/Tracking/MockLipTracker.cs
@@ -47,26 +47,28 @@ namespace Azw.FacialOsc.Tracking
         public override IDictionary<FaceKey, float> GetData()
         {
             var radian = step / 128f;
-            var sin = MathF.Sin(radian);
+            var (sin, cos) = MathF.SinCos(radian);
             var sinSq = sin * sin;
+            var positiveSin = MathF.Max(0, sin);
+            var positiveCos = MathF.Max(0, cos);
 
             return new Dictionary<FaceKey, float>() {
-                { FaceKey.Jaw_Right             , sinSq },
-                { FaceKey.Jaw_Left              , sinSq },
+                { FaceKey.Jaw_Right             , positiveSin },
+                { FaceKey.Jaw_Left              , positiveCos },
                 { FaceKey.Jaw_Forward           , sinSq },
                 { FaceKey.Jaw_Open              , sinSq },
                 { FaceKey.Mouth_Ape_Shape       , sinSq },
-                { FaceKey.Mouth_Upper_Right     , sinSq },
-                { FaceKey.Mouth_Upper_Left      , sinSq },
-                { FaceKey.Mouth_Lower_Right     , sinSq },
-                { FaceKey.Mouth_Lower_Left      , sinSq },
-                { FaceKey.Mouth_Upper_Overturn  , sinSq },
-                { FaceKey.Mouth_Lower_Overturn  , sinSq },
+                { FaceKey.Mouth_Upper_Right     , positiveSin },
+                { FaceKey.Mouth_Upper_Left      , positiveCos },
+                { FaceKey.Mouth_Lower_Right     , positiveSin },
+                { FaceKey.Mouth_Lower_Left      , positiveCos },
+                { FaceKey.Mouth_Upper_Overturn  , positiveSin },
+                { FaceKey.Mouth_Lower_Overturn  , positiveSin },
                 { FaceKey.Mouth_Pout            , sinSq },
-                { FaceKey.Mouth_Smile_Right     , sinSq },
-                { FaceKey.Mouth_Smile_Left      , sinSq },
-                { FaceKey.Mouth_Sad_Right       , sinSq },
-                { FaceKey.Mouth_Sad_Left        , sinSq },
+                { FaceKey.Mouth_Smile_Right     , positiveSin },
+                { FaceKey.Mouth_Smile_Left      , positiveSin },
+                { FaceKey.Mouth_Sad_Right       , positiveCos },
+                { FaceKey.Mouth_Sad_Left        , positiveCos },
                 { FaceKey.Cheek_Puff_Right      , sinSq },
                 { FaceKey.Cheek_Puff_Left       , sinSq },
                 { FaceKey.Cheek_Suck            , sinSq },
@@ -74,20 +76,20 @@ namespace Azw.FacialOsc.Tracking
                 { FaceKey.Mouth_Upper_UpLeft    , sinSq },
                 { FaceKey.Mouth_Lower_DownRight , sinSq },
                 { FaceKey.Mouth_Lower_DownLeft  , sinSq },
-                { FaceKey.Mouth_Upper_Inside    , sinSq },
-                { FaceKey.Mouth_Lower_Inside    , sinSq },
+                { FaceKey.Mouth_Upper_Inside    , positiveCos },
+                { FaceKey.Mouth_Lower_Inside    , positiveCos },
                 { FaceKey.Mouth_Lower_Overlay   , sinSq },
                 { FaceKey.Tongue_LongStep1      , sinSq },
                 { FaceKey.Tongue_LongStep2      , sinSq },
-                { FaceKey.Tongue_Down           , sinSq },
-                { FaceKey.Tongue_Up             , sinSq },
-                { FaceKey.Tongue_Right          , sinSq },
-                { FaceKey.Tongue_Left           , sinSq },
+                { FaceKey.Tongue_Down           , positiveSin },
+                { FaceKey.Tongue_Up             , positiveCos },
+                { FaceKey.Tongue_Right          , positiveSin },
+                { FaceKey.Tongue_Left           , positiveCos },
                 { FaceKey.Tongue_Roll           , sinSq },
-                { FaceKey.Tongue_UpLeft_Morph   , sinSq },
-                { FaceKey.Tongue_UpRight_Morph  , sinSq },
-                { FaceKey.Tongue_DownLeft_Morph , sinSq },
-                { FaceKey.Tongue_DownRight_Morph, sinSq },
+                { FaceKey.Tongue_UpLeft_Morph   , positiveSin },
+                { FaceKey.Tongue_UpRight_Morph  , positiveCos },
+                { FaceKey.Tongue_DownLeft_Morph , positiveSin },
+                { FaceKey.Tongue_DownRight_Morph, positiveCos },
             };
         }
     }
