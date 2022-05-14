691eaca5c05933597d5a4481d048073b719f63bb
Akane Sayama <a@azw.jp>
2022-05-15 00:10:35 +0900

    Mod: Prevent multiple instances

--- AkaneFacialOSC/App.xaml.cs
+++ AkaneFacialOSC/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -13,17 +15,43 @@ namespace Azw.FacialOsc
     /// </summary>
     public partial class App : Application
     {
+        const string AppName = "AZW_AkaneFacialOSC";
+        Mutex mutex = new Mutex(false, AppName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        static extern int IsIconic(IntPtr hwnd);
+
+
         public App()
         {
-            _ = Controller.Instance.InitApp();
         }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
+            _ = Controller.Instance.InitApp();
 
             DispatcherUnhandledException += OnDispatcherUnhandledException;
             TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+            if (!mutex.WaitOne(0, false))
+            {
+                var others = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+                if (others.Length > 0) {
+                    var window = others[0].MainWindowHandle;
+                    if (IsIconic(window) != 0) ShowWindow(window, 1); // SW_SHOWNORMAL 
+                    SetForegroundWindow(window);
+                }
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
         }
 
         private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
